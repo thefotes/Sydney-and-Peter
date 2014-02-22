b559738cd9f5cb1fc90b782a4f8a11272d125045
Peter Foti <peterfoti@me.com>
2014-02-22 15:05:54 -0500

    Add incident cell

--- Sydney and Peter/PFIncidentCell.h
+++ Sydney and Peter/PFIncidentCell.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface PFIncidentCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *descLabel;
 
 @end
