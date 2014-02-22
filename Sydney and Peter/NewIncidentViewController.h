a0eeb66b255c0e525e2e9298d5df2c5a3f6205d2
Peter Foti <peterfoti@me.com>
2014-02-22 15:51:54 -0500

    Add new incident view controller file

---
+++ Sydney and Peter/NewIncidentViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewIncidentViewController.h
+//  Sydney and Peter
+//
+//  Created by Peter Foti on 2/22/14.
+//  Copyright (c) 2014 Peter Foti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewIncidentViewController : UIViewController
+
+@end
---
+++ Sydney and Peter/NewIncidentViewController.m
@@ -0,0 +1,38 @@
+//
+//  NewIncidentViewController.m
+//  Sydney and Peter
+//
+//  Created by Peter Foti on 2/22/14.
+//  Copyright (c) 2014 Peter Foti. All rights reserved.
+//
+
+#import "NewIncidentViewController.h"
+
+@interface NewIncidentViewController ()
+
+@end
+
+@implementation NewIncidentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
